9dc29abcb8f3ff7ebf14fb6eac1aa9babc6ed8eb
Brady.Y.Gao <gaoyang1019@hotmail.com>
2019-09-02 22:48:23 +0800

    201909022254

---
+++ 07_apple_Enhanced.py
@@ -0,0 +1,9 @@
+# 1. input price of apple
+price = float(input("please enter the price of apple:"))
+# 2. weight of apple
+weight = float(input("please enter weight of apple:"))
+
+sum_money = price * weight
+
+money = str(sum_money)
+print("The total money you shoud pay is " + money)
---
+++ 08_format_printing.py
@@ -0,0 +1,14 @@
+
+# 1. input price of apple
+price = float(input("please enter the price of apple:"))
+# 2. weight of apple
+weight = float(input("please enter weight of apple:"))
+
+
+sum_money = price * weight
+
+print("price of apple is %.02f" % price)
+# print("\n")
+print("the weight of apple is %.02f" % weight)
+# print("\n")
+print("the total money you shoud pay is %.02f" % sum_money)
