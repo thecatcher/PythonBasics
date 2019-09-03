3246ed3c96f55f1023803274ddd45d2a71cee326
Brady.Y.Gao <gaoyang1019@hotmail.com>
2019-09-03 16:46:45 +0800

    201909031646

---
+++ 03_while_statement/17_simple_while_loop.py
@@ -0,0 +1,7 @@
+i = 1
+
+while i<=5:
+    print("hello python")
+    i = i+1
+
+print("end loop and i = %d" % i)
---
+++ 03_while_statement/19_get_even_number.py
@@ -0,0 +1,7 @@
+i = 0
+sum = 0
+while i<=100:
+    if(i%2==0):
+        sum = sum+i
+    i = i+1
+print("the summary enve number of range 0 to 100 is %d" % sum)
